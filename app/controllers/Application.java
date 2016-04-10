31ebf1ee73b2774d599dfe3489e6c631b0a00f89
Ramya <ramyakrishna5711@gmail.com>
2016-04-10 02:14:57 -0700

    added recommendations + session handling

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -31,28 +31,33 @@ import models.Products;
 import uk.co.panaxiom.playjongo.*;
 
 public class Application extends Controller {
-	
+
 	public  String status = "success";
 
 	public List<String> list;
 	public List<String> names;
-	
+
 	public Result index() {
 		return ok(index.render("Hello :)"));
 	}
 
 	// Sravya Dara's Additions
-	
+
 	public Result adminRetailerCatalog() {
-		return ok(adminRetailerCatalog.render("Your new application is ready."));
+		if(session("username") != null) {
+			return ok(adminRetailerCatalog.render("Your new application is ready."));
+		}else {
+			status = "success";
+			return viewLogin();
+		}
 
 	}
-	
+
 	public Result viewSignUpForm(){
 		Form<SignUpForm> signUpForm = Form.form(SignUpForm.class);
 		return ok(signup.render(signUpForm));
 	}
-	
+
 	public Result signUp(){
 		Form<SignUpForm> signUp = Form.form(SignUpForm.class);
 		SignUpForm form = signUp.bindFromRequest().get();
@@ -63,56 +68,65 @@ public class Application extends Controller {
 				form.getEmailId(),  form.getContactNum(),
 				form.getUserName(), form.getPassword(),
 				form.getRetailer(), "pending");
-		
+
 		System.out.println(newUser.userName);
 		newUser.insert();
 		return viewLogin();
-		
+
 	}
 
 	public Result imageCatalog(String id) {
-		Products prod = new Products();
-		MongoCursor<Products> cursor = prod.findall();
-		List<ProductDetails> details = new ArrayList<ProductDetails>();
-		while (cursor.hasNext()) {
-			List<ProductDetails> temp = cursor.next().products;
-			for (ProductDetails pd : temp) {
-				if (pd.getRetailer().equalsIgnoreCase(id)) {
-					System.out.println("printing images");
-					System.out.println(pd.getImage());
-					details.add(pd);
+		if(session("username") != null) {
+			Products prod = new Products();
+			MongoCursor<Products> cursor = prod.findall();
+			List<ProductDetails> details = new ArrayList<ProductDetails>();
+			while (cursor.hasNext()) {
+				List<ProductDetails> temp = cursor.next().products;
+				for (ProductDetails pd : temp) {
+					if (pd.getRetailer().equalsIgnoreCase(id)) {
+						System.out.println("printing images");
+						System.out.println(pd.getImage());
+						details.add(pd);
+					}
 				}
 			}
+			return ok(catalogImagesA.render(id, details));
+		}else {
+			status = "success";
+			return viewLogin();
 		}
-		return ok(catalogImagesA.render(id, details));
-
 	}
 
 	public Result adminInbox() {
-	 Users users = new Users();
-	 MongoCursor<Users> cursor = users.findByStatus("pending");
-	 List<Users> userlist = new ArrayList<Users>();
-	 while(cursor.hasNext()){
-		 userlist.add(cursor.next());
-	 }
-	
-		return ok(adminInbox.render(userlist));
+		if(session("username") != null) {
+			Users users = new Users();
+			MongoCursor<Users> cursor = users.findByStatus("pending");
+			List<Users> userlist = new ArrayList<Users>();
+			while(cursor.hasNext()){
+				userlist.add(cursor.next());
+			}
+
+			return ok(adminInbox.render(userlist));
+		}else {
+			status = "success";
+			return viewLogin();
+		}
 	}
-	
+
 	public Result updateUserStatus(String status,String userName){
 		System.out.println(status);
 		System.out.println(userName);
 		System.out.println("inside loop");
-        Users users = new Users();
-        users = Users.findById(userName);
-        System.out.println(users.lastName);
-        users.status = status;
-        users.update();
+		Users users = new Users();
+		users = Users.findById(userName);
+		System.out.println(users.lastName);
+		users.status = status;
+		users.update();
 		return adminInbox();
-      
+
 	}
 
-	
+
 	// Susmitha's additions
 
 	public Result viewLogin() {
@@ -129,7 +143,7 @@ public class Application extends Controller {
 		System.out.println("Current user session: "+ login.getUsername());
 		System.out.println("user password : " + login.getPassword());
 		session("username", login.getUsername());
-		
+
 		Users user = Users.findByName(login.getUsername(), login.getPassword());
 		System.out.println("User value : " + user);
 		if(user != null) {
@@ -141,7 +155,7 @@ public class Application extends Controller {
 			}else if(user.role.equalsIgnoreCase("customer")){
 				return listofCategories();
 			}
-		
+
 		}else {
 			System.out.println("Invalid Credentials");
 			status = "failed";
@@ -149,12 +163,12 @@ public class Application extends Controller {
 		}
 		return ok();
 	}
-	
+
 	public Result logout() {
-        session().clear();
-        System.out.println("Session username : " + session("username"));
-        System.out.println("Session retailer name : " + session("retailerName"));
-        status = "relogin";
+		session().clear();
+		System.out.println("Session username : " + session("username"));
+		System.out.println("Session retailer name : " + session("retailerName"));
+		status = "relogin";
 		return viewLogin();
 	}
 
@@ -162,7 +176,12 @@ public class Application extends Controller {
 	public Result showProductFormData() {
 		System.out.println("Inside show form");
 		Form<ProductForm> formData = Form.form(ProductForm.class);
-		return ok(retailer_add_product.render(formData));
+		if(session("retailerName") != null) {
+			return ok(retailer_add_product.render(session("retailerName"),formData));
+		}else {
+			status = "success";
+			return viewLogin();
+		}
 	}
 
 	public Result showRetailerCategories(){
@@ -171,14 +190,18 @@ public class Application extends Controller {
 
 		// Fetching categories list
 		List<String> categoryArray = p.getCategories("categoryname");
-
-		return ok(retailer_categories.render(categoryArray));
+		if(session("retailerName") != null) {
+			return ok(retailer_categories.render(session("retailerName"), categoryArray));
+		}else {
+			status = "success";
+			return viewLogin();
+		}
 	}
-	
+
 	public String shortUUID() {
-		  UUID uuid = UUID.randomUUID();
-		  long l = ByteBuffer.wrap(uuid.toString().getBytes()).getLong();
-		  return Long.toString(l, Character.MAX_RADIX);
+		UUID uuid = UUID.randomUUID();
+		long l = ByteBuffer.wrap(uuid.toString().getBytes()).getLong();
+		return Long.toString(l, Character.MAX_RADIX);
 	}
 
 	public Result postProductFormData() {
@@ -235,170 +258,195 @@ public class Application extends Controller {
 	public Result showRetailerProducts(String categoryName){
 
 		//fetching products list for specific retailer.
-
-		MongoCursor<Products> cursor = Products.findByName(categoryName);
-		List<ProductDetails> filteredProductDetails = new ArrayList<ProductDetails>();
-		while(cursor.hasNext()){
-			List<ProductDetails> details = cursor.next().products;
-			for(ProductDetails d : details) {
-				if(d.retailer.equalsIgnoreCase(session("retailerName"))){
-					filteredProductDetails.add(d);
+		if(session("retailerName") != null) {
+			MongoCursor<Products> cursor = Products.findByName(categoryName);
+			List<ProductDetails> filteredProductDetails = new ArrayList<ProductDetails>();
+			while(cursor.hasNext()){
+				List<ProductDetails> details = cursor.next().products;
+				for(ProductDetails d : details) {
+					if(d.retailer.equalsIgnoreCase(session("retailerName"))){
+						filteredProductDetails.add(d);
+					}
 				}
-			}
+			}		
+			return ok(retailer_products.render(session("retailerName"), filteredProductDetails));
+		}else {
+			status = "success";
+			return viewLogin();
 		}
-
-		return ok(retailer_products.render(filteredProductDetails));
 	}
 
 	// Sravya Reddy Additions
 
 	public Result listofCategories(){
 
-		Products p = new Products();
-		System.out.println("Printng number of rows in products collections : " + p.getCount() + " and " + p.findall().count() );
-		MongoCursor<Products> mongoCursor = p.findall();
-		List<String> categoryArray = p.getCategories("categoryname");
-		System.out.println("Printing Distinct values : " + p.getCategories("categoryname").size());
-		for(String s : p.getCategories("categoryname")) {
-			System.out.println("Category : "+ s);
+		if(session("username") != null) {
+			Products p = new Products();
+			System.out.println("Printng number of rows in products collections : " + p.getCount() + " and " + p.findall().count() );
+			MongoCursor<Products> mongoCursor = p.findall();
+			List<String> categoryArray = p.getCategories("categoryname");
+			System.out.println("Printing Distinct values : " + p.getCategories("categoryname").size());
+			for(String s : p.getCategories("categoryname")) {
+				System.out.println("Category : "+ s);
+			}
+			List<ProductDetails> recommendedProductsList = getRecommendations();
+			System.out.println("Count of recommended items : " + recommendedProductsList.size() );
+			return ok(users_main.render(session("username").toUpperCase(), categoryArray, recommendedProductsList));
+		}else {
+			status = "success";
+			return viewLogin();
 		}
-		List<ProductDetails> recommendedProductsList = getRecommendations();
-		System.out.println("Count of recommended items : " + recommendedProductsList.size() );
-		return ok(users_main.render(categoryArray, recommendedProductsList));
 	}
 
 
 	public Result listofProducts(String categoryname){
-		Products p = new Products();
-		MongoCursor<Products> m = p.findByName(categoryname);
-		List<ProductDetails> proddetails = new ArrayList<ProductDetails>();
-		while(m.hasNext()){
+		if(session("username") != null) {
+			Products p = new Products();
+			MongoCursor<Products> m = p.findByName(categoryname);
+			List<ProductDetails> proddetails = new ArrayList<ProductDetails>();
+			while(m.hasNext()){
 
-			List<ProductDetails> productDetails  = m.next().products;
-			for(ProductDetails pd : productDetails){
-				String name = pd.getName();
-				String url= pd.getQrcode();
-				String prdid=pd.getProductid();
-				proddetails.add(pd);
-			}
-		}
-		return ok(user_products.render(proddetails));
-	}
-//Sravya Reddy
-	public Result productdetailsfunc(String categoryname, String prdid,String liked){
-		//int prodid =Integer.parseInt(prdid);
-		System.out.println("&&& Inside productdetailsfunction &&&");
-		String retailer="";
-		String description="";
-		String price="";
-		String imageurl= "";
-		String qrcode="";
-		String name="";
-		String type="";//---for similar items
-		String productid="";
-		String like="visible";
-        Products p = new Products();
-		MongoCursor<Products> m = p.findByName(categoryname);
-		while(m.hasNext()){
-
-			List<ProductDetails> prodDetails  = m.next().products;
-			System.out.println(" After prodDetails :*******" + prodDetails.size());
-			for(ProductDetails pd : prodDetails){
-				
-				
-				if(pd.getProductid().equalsIgnoreCase(prdid))
-				{ 	
-					productid=pd.getProductid();
-					retailer = pd.getRetailer();
-					description = pd.getDescription();
-					price = Integer.toString(pd.getPrice());
-					imageurl= pd.getImage();
-					qrcode= pd.getQrcode();
-					name = pd.getName();
-                    type = pd.getType();
-                    System.out.println("Inside product details type : "+ type);
-					String message = 	description +"/"+ price +"/"+ retailer;
-					System.out.println("Message : " + message);
-					
-					
-					String username=session("username");
-					System.out.println("User from session: "+username);
-					Users users = new Users();
-			        users = Users.findById(username);
-			        List<String> interestedItems = users.interested;
-					for(String likeItem:interestedItems){
-						if(likeItem.equalsIgnoreCase(productid)){
-							like="disabled";
-							System.out.println("------Already Liked-------");
-						}
-					}
-
-				}
-			}
-		}
-		
-		Products p1 = new Products();
-		MongoCursor<Products> m1 = p1.findByName(categoryname);
-		List<ProductDetails> proddetails = new ArrayList<ProductDetails>();
-		while(m1.hasNext()){
-
-			List<ProductDetails> productDetails1  = m1.next().products;
-			for(ProductDetails pd : productDetails1){
-				
-				System.out.println(" Inside carousal function ");
-				if((pd.getType().equalsIgnoreCase(type)) && !pd.getProductid().equalsIgnoreCase(prdid)){
+				List<ProductDetails> productDetails  = m.next().products;
+				for(ProductDetails pd : productDetails){
+					String name = pd.getName();
+					String url= pd.getQrcode();
+					String prdid=pd.getProductid();
 					proddetails.add(pd);
-					System.out.println(" Inside carousal if condition ");
-					System.out.print(" product type: " + pd.type + "product ID: " +pd.productid);
 				}
-			//proddetails.add(pd);
 			}
+			return ok(user_products.render(session("username").toUpperCase(), proddetails));
+		}else {
+			status = "success";
+			return viewLogin();
 		}
-		if(liked.equalsIgnoreCase("true")){
-			System.out.println("****Liked*******");	
-			String username=session("username");
-			System.out.println("User from session: "+username);
-			Users users = new Users();
-	        users = Users.findById(username);
-	        System.out.println("Interested list size: "+users.interested.size());
-	        users.interested.add(prdid);
-	        users.update();
-	        like="disabled";
+	}
+	//Sravya Reddy
+	public Result productdetailsfunc(String categoryname, String prdid,String liked){
 
-			
+		if(session("username") != null) {
+			//int prodid =Integer.parseInt(prdid);
+			System.out.println("&&& Inside productdetailsfunction &&&");
+			String retailer="";
+			String description="";
+			String price="";
+			String imageurl= "";
+			String qrcode="";
+			String name="";
+			String type="";//---for similar items
+			String productid="";
+			String like="visible";
+			Products p = new Products();
+			MongoCursor<Products> m = p.findByName(categoryname);
+			while(m.hasNext()){
+
+				List<ProductDetails> prodDetails  = m.next().products;
+				System.out.println(" After prodDetails :*******" + prodDetails.size());
+				for(ProductDetails pd : prodDetails){
+
+
+					if(pd.getProductid().equalsIgnoreCase(prdid))
+					{ 	
+						productid=pd.getProductid();
+						retailer = pd.getRetailer();
+						description = pd.getDescription();
+						price = Integer.toString(pd.getPrice());
+						imageurl= pd.getImage();
+						qrcode= pd.getQrcode();
+						name = pd.getName();
+						type = pd.getType();
+						System.out.println("Inside product details type : "+ type);
+						String message = 	description +"/"+ price +"/"+ retailer;
+						System.out.println("Message : " + message);
+
+
+						String username=session("username");
+						System.out.println("User from session: "+username);
+						Users users = new Users();
+						users = Users.findById(username);
+						List<String> interestedItems = users.interested;
+						for(String likeItem:interestedItems){
+							if(likeItem.equalsIgnoreCase(productid)){
+								like="disabled";
+								System.out.println("------Already Liked-------");
+							}
+						}
+
+					}
+				}
+			}
+
+			Products p1 = new Products();
+			MongoCursor<Products> m1 = p1.findByName(categoryname);
+			List<ProductDetails> proddetails = new ArrayList<ProductDetails>();
+			while(m1.hasNext()){
+
+				List<ProductDetails> productDetails1  = m1.next().products;
+				for(ProductDetails pd : productDetails1){
+
+					System.out.println(" Inside carousal function ");
+					if((pd.getType().equalsIgnoreCase(type)) && !pd.getProductid().equalsIgnoreCase(prdid)){
+						proddetails.add(pd);
+						System.out.println(" Inside carousal if condition ");
+						System.out.print(" product type: " + pd.type + "product ID: " +pd.productid);
+					}
+					//proddetails.add(pd);
+				}
+			}
+			if(liked.equalsIgnoreCase("true")){
+				System.out.println("****Liked*******");	
+				String username=session("username");
+				System.out.println("User from session: "+username);
+				Users users = new Users();
+				users = Users.findById(username);
+				System.out.println("Interested list size: "+users.interested.size());
+				users.interested.add(prdid);
+				users.update();
+				like="disabled";
+
+
+			}
+			return ok(product_screen.render(session("username").toUpperCase(),productid,description,price,retailer,imageurl,qrcode,name,proddetails,like));
+		}else {
+			status = "success";
+			return viewLogin();
 		}
-		return ok(product_screen.render(productid,description,price,retailer,imageurl,qrcode,name,proddetails,like));
 	}
 	//Sravya Reddy
 	/*public void likeFunctionPressed(String likedItem){
 		System.out.println("Liked*******");
 		//return ok();
-		
+
 	}*/
 	public Result searchfunc(String categoryname,String searchvalue){
 
-		List<ProductDetails> searchdetails = new ArrayList<ProductDetails>();
+		if(session("username") != null) {
 
-		Products p = new Products();
-		MongoCursor<Products> m = p.findByName(categoryname);
-		while(m.hasNext()){
-			List<ProductDetails> prodDetails  = m.next().products;
-			for(ProductDetails pd : prodDetails){
-				String thisName = pd.getName();
-				String thisDesc = pd.getDescription();
-				String thisRet = pd.getRetailer();
-				String matchMe = "(?i).*"+searchvalue+ ".*";
-				if (thisName.matches(matchMe) || thisDesc.matches(matchMe) ||
-						thisRet.matches(matchMe))
-				{
-					searchdetails.add(pd);
+			List<ProductDetails> searchdetails = new ArrayList<ProductDetails>();
+
+			Products p = new Products();
+			MongoCursor<Products> m = p.findByName(categoryname);
+			while(m.hasNext()){
+				List<ProductDetails> prodDetails  = m.next().products;
+				for(ProductDetails pd : prodDetails){
+					String thisName = pd.getName();
+					String thisDesc = pd.getDescription();
+					String thisRet = pd.getRetailer();
+					String matchMe = "(?i).*"+searchvalue+ ".*";
+					if (thisName.matches(matchMe) || thisDesc.matches(matchMe) ||
+							thisRet.matches(matchMe))
+					{
+						searchdetails.add(pd);
+					}
 				}
-			}
 
+			}
+			return ok(user_products.render(session("username").toUpperCase(),searchdetails));
+		}else {
+			status = "success";
+			return viewLogin();
 		}
-		return ok(user_products.render(searchdetails));
 	}
-	
+
 	public List<ProductDetails> getRecommendations() {
 		System.out.println("Current logged user : " + session("username"));
 		String loggedUser = session("username");
@@ -418,7 +466,7 @@ public class Application extends Controller {
 				recommendationSet.addAll(othersInterested);
 			}
 		}
-		
+
 		System.out.println("Printing my hash set value : " + recommendationSet);
 		recommendationSet.removeAll(currentUserInterested);
 		System.out.println("Printing after hash set value : " + recommendationSet);
@@ -427,7 +475,7 @@ public class Application extends Controller {
 		while(iterator.hasNext()) {
 			String pid = (String) iterator.next();
 			System.out.println("Iterator current item : " + pid);
-			
+
 			Iterable<Products> rsi = Products.findProduct(pid);
 			while(rsi.iterator().hasNext()) {
 				System.out.println("----------- Inside rsi -----------");
@@ -437,7 +485,7 @@ public class Application extends Controller {
 		}
 		return productDetailsArray;
 	}
-	
+
 	public int findMatchPercentage(ArrayList<String> c, ArrayList<String> o) {
 		int initialSize = c.size();
 		c.retainAll(o);
